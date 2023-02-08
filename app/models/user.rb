7dbea617b9e8c38cc31b39302b9473d6a6dbd7a5
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-02-08 13:26:09 +0000

    done 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,36 +5,49 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
-  
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
-  
-  
+
+
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def follow(user)
     relationships.create(followed_id: user.id)
   end
-  
+
   def unfollow(user)
     relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
-  
+
+
+  def self.search_for(content, method)
+    if method == "perfect"
+      User.where(name: content)
+    elsif method == "forward"
+      User.where("name LIKE ?", content + "%")
+    elsif method == "backward"
+      User.where("name LIKE ?", "%" + content)
+    else
+      User.where("name LIKE ?", "%" + content + "%")
+    end
+  end
+
 end
